68f5fd7171c959d7debbe319791718f9bafded1d
Zahoor Abbas <zahoorabbas360@gmail.com>
2024-02-01 01:55:14 +0500

    ui fixes

--- src/pages/TextCard.jsx
+++ src/pages/TextCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Switch, Typography } from '@mui/material'
+import { Box, Button, FormControlLabel, FormGroup, Grid, Switch, Typography } from '@mui/material'
 import React from 'react'
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
@@ -6,10 +6,12 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 function TextCard() {
   return (
     <Box >
-         <Accordion style={{ background: 'white', color:"black",border:"1px solid #ccc",margin:"20px" }}>
+         <Accordion defaultExpanded style={{ background: 'white', color:"black",border:"1px solid #ccc",margin:"20px" }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon sx={{color:"black"}} />}
           aria-controls="panel2-content"
@@ -24,9 +26,15 @@ function TextCard() {
             The patient did not provide any specific chief complaint, history of present illness, or relevant information about their symptoms, feelings, and experiences during this visit. The conversation consisted of unrelated topics and personal anecdotes. Further assessment is needed to determine the patient's medical concerns and needs.
             </Typography>
          </Box>
-         <Grid marginTop={2} display="flex" justifyContent="end">
-<Box>
-    <Switch/>
+         <Grid marginTop={2} display="flex" justifyContent="space-between">
+<Box sx={{display:"flex",alignItems:"center"}}>
+<FormGroup>
+  <FormControlLabel control={<Switch />} label={<Typography variant='body2'>Full Transcript</Typography>} />
+</FormGroup>
+<Box sx={{display:"flex",gap:"10px"}}>
+    <ThumbUpOffAltIcon/>
+    <ThumbDownOffAltIcon/>
+</Box>
 </Box>
          <Button variant='outlined' size='small' startIcon={<ContentCopyIcon/>}>Copy all</Button>
          </Grid>
